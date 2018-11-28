bd0ec659654a9275394bafdd373254cad2af9e2e
Benjie Genchel <benjiegenchel@gmail.com>
2018-11-28 13:48:21 -0500

    getting bebop dataset working

--- src/data/parsing/datasets.py
+++ src/data/parsing/datasets.py
@@ -83,6 +83,9 @@ class MidiTicksDataset(Dataset):
                         formatted_measure = self._get_empty_ticks(96)
                         sequence += formatted_measure
 
+                # import pdb
+                # pdb.set_trace()
+                sequence = list(np.array(sequence).argmax(axis=1))
                 if target_type == "full_sequence":
                     target = sequence
                 else: # target_type == "next_step"
